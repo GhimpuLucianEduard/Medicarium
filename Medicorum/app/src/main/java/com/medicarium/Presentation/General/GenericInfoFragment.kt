a43f9fdc22b2b49dfc154de831aec9915dc0d78d
Lucian Ghimpu <Lucian.Ghimpu@Yardi.Com>
2019-06-07 17:43:21 +0300

    image switcher

--- Medicorum/app/src/main/java/com/medicarium/Presentation/General/GenericInfoFragment.kt
+++ Medicorum/app/src/main/java/com/medicarium/Presentation/General/GenericInfoFragment.kt
@@ -1,13 +1,14 @@
 package com.medicarium.Presentation.General
 
 
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.medicarium.Presentation.BaseFragment
-
 import com.medicarium.R
+import kotlinx.android.synthetic.main.fragment_generic_info.*
 
 class GenericInfoFragment : BaseFragment() {
 
@@ -20,4 +21,19 @@ class GenericInfoFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_generic_info, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navBar.setLeftImageDrawnable(resources.getDrawable(R.drawable.empty_drawable))
+        navBar.setRightImageDrawnable(resources.getDrawable(R.drawable.more_verti))
+        navBar.setTitletext("")
+
+        setupImageSwitchers()
+    }
+
+    private fun setupImageSwitchers() {
+
+        firstNameImageSwitcher.setOnClickListener {
+            (it.background as TransitionDrawable).reverseTransition(0)
+        }
+    }
 }
